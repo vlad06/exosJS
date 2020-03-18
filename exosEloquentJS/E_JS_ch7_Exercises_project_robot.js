19e6e4d6baaa0c0345a449aff1c731e416e15490
vlad06 <vladimirhuppert@hotmail.fr>
2020-03-18 21:23:18 +0100

    added beginning of exercises with the pathfinder robot on eloquent JS

--- exosEloquentJS/E_JS_ch7_Exercises_project_robot.js
+++ exosEloquentJS/E_JS_ch7_Exercises_project_robot.js
@@ -1,3 +1,7 @@
+
+//========================================================================================
+//=============================     EXERCISES       ======================================
+//========================================================================================
 const roads = [
     "Alice's House-Bob's House", "Alice's House-Cabin",
     "Alice's House-Post Office", "Bob's House-Town Hall",
@@ -26,8 +30,6 @@ function buildGraph(edges) {
 }
 
 const roadGraph = buildGraph(roads);
-
-// console.log(roadGraph);
 
 class VillageState {
     constructor(place, parcels) {
@@ -63,35 +65,23 @@ class VillageState {
     }
 }
 
-// let first = new VillageState(
-//     "Post Office",                                      // The robot will begin at the Post Office
-//     [{place: "Post Office", address: "Alice's House"}]  // the parcel is at the Post Office and must be delivered to Alice's House
-// );
-// // console.log(first);
-// let next = first.move("Alice's House");
-// // console.log(next);
-// console.log(next.place);            // Alice's House
-// console.log(next.parcels);          // []  // the move causes the parcel to be delivered
-// console.log(first.place);           //
-
 function runRobot(state, robot, memory) {
     for(let turn = 0;; turn++) {
         if(state.parcels.length == 0) {
-            console.log(`Done in ${turn} turns`);
-            break;
+            // console.log(`Done in ${turn} turns`);
+            return turn;
         }
         let action = robot(state, memory);
             state = state.move(action.direction);
             memory = action.memory;
-            console.log(`Moved to ${action.direction}`);
+            // console.log(`Moved to ${action.direction}`);
     }
 }
 
-console.log("============================================================");
-console.log("=====================    RANDOM ROBOT   ====================");
-console.log("============================================================");
-// ===========================================================
-// IF WE MAKE IT RANDOMLY
+// console.log("============================================================");
+// console.log("=====================    RANDOM ROBOT   ====================");
+// console.log("============================================================");
+
 function randomPick(array) {
     let choice = Math.floor(Math.random() * array.length);
     return array[choice];
@@ -101,37 +91,18 @@ function randomRobot(state) {
     return {direction: randomPick(roadGraph[state.place])};
 }
 
-// VillageState.random = function(parcelCount = 5) {
-//     let parcels = [];
-//     for(let i = 0; i < parcelCount; i++) {
-//         let address = randomPick(Object.keys(roadGraph));
-//         let place;
-//         do {
-//             place = randomPick(Object.keys(roadGraph));
-//         } while(place == address);
-//         parcels.push({place, address});
-//     }
-//     return new VillageState("Post Office", parcels);
-// }
-
 runRobot(VillageState.random(), randomRobot);
 
-console.log("==========================================================================");
-console.log("=========================   MAIL TRUCK ROBOT    ==========================");
-console.log("==========================================================================");
-//===========================================================
-// THE MAIL TRUCK'S ROUTE (or running twice a route that passes all places in the village)
+// console.log("==========================================================================");
+// console.log("=========================   MAIL TRUCK ROBOT    ==========================");
+// console.log("==========================================================================");
 
-// below is such a route (starting from the post office)
 const mailRoute = [ 
     "Alice's House", "Cabin", "Alice's House", "Bob's House",
     "Town Hall", "Daria's House", "Ernie's House",
     "Grete's House", "Shop", "Grete's House", "Farm",
     "Marketplace", "Post Office"
 ];
-
-// To implement the route-following robot, we'll need to make use of robot memory. The robot keeps
-// the rest of its route in its memory and drops the first element every turn.
 
 function routeRobot(state, memory) {
     if(memory.length == 0) {
@@ -142,25 +113,23 @@ function routeRobot(state, memory) {
 
 runRobot(VillageState.random(), routeRobot, []);
 
-console.log("==========================================================================");
-console.log("=========================   PATHFINDER ROBOT    ==========================");
-console.log("==========================================================================");
-//================================================================
-// PATHFINDING
+// console.log("==========================================================================");
+// console.log("=========================   PATHFINDER ROBOT    ==========================");
+// console.log("==========================================================================");
 
 function findRoute(graph, from, to) {
-    let work = [{at: from, route: []}];
-    for(let i = 0; i < work.length; i++) {
-        let {at, route} = work[i];
-        for(let place of graph[at]) {
-            if(place == to) {
-                return route.concat(place);
-            }
-            if(!work.some(w => w.at == place)) {
-                work.push({at: place, route: route.concat(place)});
-            }
-        }
+  let work = [{at: from, route: []}];
+  for(let i = 0; i < work.length; i++) {
+    let {at, route} = work[i];
+    for(let place of graph[at]) {
+      if(place == to) {
+        return route.concat(place);
+      }
+      if(!work.some(w => w.at == place)) {
+          work.push({at: place, route: route.concat(place)});
+      }
     }
+  }
 }
 
 function goalOrientedRobot({place, parcels}, route) {
@@ -176,3 +145,24 @@ function goalOrientedRobot({place, parcels}, route) {
 }
 
 runRobot(VillageState.random(), goalOrientedRobot, []);
+
+//--------------------------------------------------------------------------------------
+// MEASURING A ROBOT
+
+function compareRobots(firstRobot, secondRobot) {
+	let firstResult = 0;
+	let secondResult = 0;
+	for(let i = 0; i < 100; i++) {
+		let randomState = VillageState.random();
+		firstResult += runRobot(randomState, firstRobot, []);
+		secondResult += runRobot(randomState, secondRobot, []);
+	}
+	return {robotOne: firstResult / 100,
+					robotTwo:	secondResult / 100};
+}
+
+console.log(compareRobots(randomRobot, routeRobot));
+console.log(compareRobots(randomRobot, goalOrientedRobot));
+console.log(compareRobots(routeRobot, goalOrientedRobot));
+
+//---------------------------------------------------------------------------------------
