2878a9c4681cbfe55481b7a1343ae7d31a1bbb2b
vlad06 <vladimirhuppert@hotmail.fr>
2020-03-24 16:48:19 +0100

    beggining of a little accordeon with js

---
+++ accordeon/accordeon_1.js
@@ -0,0 +1,9 @@
+let myAccordeon = document.getElementsByClassName("accordeon");
+
+for(let i = 0; i < myAccordeon.length; i++) {
+    myAccordeon[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;        // the panel variable reference the p element here
+        panel.style.display = (panel.style.display === "block") ? "none" : "block";
+    });
+}
